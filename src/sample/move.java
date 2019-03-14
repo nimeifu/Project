a156f0be69e0f649e120666f88146fa5d4cf784a
serlinecai4935 <scai4935@bths.edu>
2019-03-14 10:10:41 -0400

    212

--- src/sample/move.java
+++ src/sample/move.java
@@ -1,50 +1,13 @@
 package sample;
+import javafx.*;
 
+import javax.swing.text.html.ImageView;
 import java.awt.*;
-import java.awt.event.KeyEvent;
 
-public class move {
-    Dimension screesize=Toolkit.getDefaultToolkit().getScreenSize();
-    private double Width=screesize.getWidth()/2;
-    private double Height=screesize.getHeight()/1.5;
-    public double x;
-
-    public move(double width,double height)
-    {
-        this.Width=width;
-        this.Height=height;
-    }
-    public void keyPressed(KeyEvent e)
-    {
-        int key=e.getKeyCode();
-
-        if(key== KeyEvent.VK_LEFT)
-        {
-            this.Width +=-2;
-        }
-
-        if(key==KeyEvent.VK_RIGHT)
-        {
-            this.Width +=2;
-        }
-
-        if(key==KeyEvent.VK_LEFT && key==KeyEvent.VK_SHIFT)
-        {
-            this.Width+=-4;
-        }
-
-        if(key==KeyEvent.VK_RIGHT && key==KeyEvent.VK_SHIFT)
-        {
-            this.Width+=4;
-        }
-    }
-
-    public void action()
-    {
-        x+=Width;
-    }
-
-
+public abstract class move
+{
+    Image image;
+    ImageView iv;
 
 
 }
