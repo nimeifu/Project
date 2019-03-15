95743ee7da977f9fb4b1ec28b08cc98ec8a953dc
nimeifu <gliu3637@bths.edu>
2019-03-14 23:59:45 -0400

    changes

--- src/sample/move.java
+++ src/sample/move.java
@@ -1,5 +1,6 @@
 package sample;
 import javafx.*;
+import javafx.scene.layout.Pane;
 
 import javax.swing.text.html.ImageView;
 import java.awt.*;
@@ -8,6 +9,19 @@ public abstract class move
 {
     Image image;
     ImageView iv;
+    Pane layer;
+    double x;
+    double y;
+    double dx;
+    double dy;
+    double endurance;
+    double damage;
+
+    public move(Pane layer,Image image,double x,double y,double dx,double dy,double endurance,double damage)
+    {
+        this.layer=layer;
+    }
+
 
 
 }
